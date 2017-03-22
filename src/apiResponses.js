e7ca19ee2cd8b724f9f99bc6dbfe8512d83b9d49
srartese <saa2457@rit2457@rit.edu>
2017-03-22 13:44:23 -0400

    Final push for eduwine

--- src/apiResponses.js
+++ src/apiResponses.js
@@ -1,8 +1,13 @@
+const crypto = require('crypto');
 const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const json = fs.readFileSync(`${__dirname}/../src/catalog.json`);
+const css = fs.readFileSync(`${__dirname}/../client/global.css`);
 const parsedJson = JSON.parse(json);
+const wines = {};
+const etag = crypto.createHash('sha1').update(JSON.stringify(wines));
+const digest = etag.digest('hex');
 
 const setIndex = (request, response, contentType) => {
   response.writeHead(200, { 'Content-Type': contentType });
@@ -10,13 +15,113 @@ const setIndex = (request, response, contentType) => {
   response.end();
 };
 
+const getCSS = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(css);
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
 // responds with json object
 const sendCatalog = (request, response, file, contentType) => {
   response.writeHead(200, { 'Content-Type': contentType });
   response.write(JSON.stringify(file));
-  // console.log(typeof file);
   // console.log(JSON.stringify(file));
   response.end();
+};
+
+const setDetails = (request, response, file, contentType, body) => {
+  response.writeHead(200, { 'Content-Type': contentType, etag: digest });
+
+  const multJSON = JSON.stringify({
+    bodyJn: body,
+    fileJn: file,
+  });
+  response.write(multJSON);
+  response.end();
+};
+
+// This is called to send requests for methods that dont need special constructions
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// function to add a user from a POST body
+const addWine = (request, response, body) => {
+  // default json message
+  const responseJSON = {
+    message: 'Name is required.',
+  };
+
+  if (!body.name) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  // default status code to 201 created
+  let responseCode = 201;
+
+  // if that user's name already exists in our object
+  // then switch to a 204 updated status
+  if (wines[body.name]) {
+    responseCode = 204;
+  } else {
+    // otherwise create an object with that name
+    wines[body.name] = {};
+  }
+
+  // add or update fields for this user name
+  wines[body.name].name = body.name;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  return respondJSONMeta(request, response, responseCode);
+};
+
+
+// function for 404 not found requests with message
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const getWineMeta = (request, response) => {
+  if (request.headers['if-none-match'] === digest) {
+    return respondJSONMeta(request, response, 304);
+  }
+
+  // return 200 without message, just the meta data
+  return respondJSONMeta(request, response, 200);
+};
+
+
+// function for 404 not found without message
+const notFoundMeta = (request, response) => {
+  // return a 404 without an error message
+  respondJSONMeta(request, response, 404);
 };
 
 const getIndex = (request, response) => {
@@ -27,7 +132,18 @@ const getCatalog = (request, response) => {
   sendCatalog(request, response, parsedJson, 'application/json');
 };
 
+const getDetails = (request, response, body) => {
+  setDetails(request, response, parsedJson, 'application/json', body);
+};
+
+
 module.exports = {
   getIndex,
+  getCSS,
   getCatalog,
+  getDetails,
+  notFound,
+  notFoundMeta,
+  addWine,
+  getWineMeta,
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 
 const apiHandler = require('./apiResponses.js');
 
@@ -9,17 +10,100 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const handleGet = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/') {
     apiHandler.getIndex(request, response);
-    // apiHandler.getCatalog(request, response);
-  } else {
+    // apiHandler.getCatalog(request, response)
+  } else if (parsedUrl.pathname === '/ref') {
+    apiHandler.checkRefresh(request, response);
+  } else if (parsedUrl.pathname === '/global.css') {
+    apiHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/~cat') {
     apiHandler.getCatalog(request, response);
+  } else {
+    apiHandler.notFound(request, response);
+  }
+};
+
+// Handle Post requests
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/det') {
+    const res = response;
+
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+   // pass to our getDetails function
+      apiHandler.getDetails(request, res, bodyParams);
+    });
+  } else if (parsedUrl.pathname === '/addWine') {
+    const res = response;
+
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      // pass to our addUser function
+      apiHandler.addWine(request, res, bodyParams);
+    });
+  } else {
+    apiHandler.notFound(request, response);
+  }
+};
+
+// handle HEAD requests
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getWine') {
+    apiHandler.getWineMeta(request, response);
+  } else {
+    apiHandler.notFoundMeta(request, response);
+  }
+};
+
+
+const handleErr = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/notReal') {
+    apiHandler.notFound(request, response);
   }
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-  if (request.method === 'GET') {
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'GET') {
     handleGet(request, response, parsedUrl);
-  } else { handleGet(request, response, parsedUrl); }
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
+  } else {
+    apiHandler.notFound(request, response);
+    handleErr(request, response, parsedUrl);
+  }
+  console.log(request.method);
 };
 
 http.createServer(onRequest).listen(port);
